8ca0c1865615eeb2e849bbbc3b1b046cd2e89e9a
jackcornblum <jcornblum@gmail.com>
2021-05-09 16:15:28 -0400

    created basic structure for page

--- script.js
+++ script.js
@@ -4,21 +4,40 @@ h2.textContent = "This content added by JavaScript";
 // get the modal
 var modal = document.getElementById("modal1");
 
-var btn = document.getElementById("btnOne");
+var modalTwo = document.getElementById("modal2");
+
+var btn1 = document.getElementById("btnOne");
+
 
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-    modal.style.display = "block";
+//btn.onclick = function() {
+  //  modal.style.display = "block";
+//}
+
+function btnClick(element) {
+    if (element.id === 'btnOne') {
+        modal.style.display="block";
+    }
+    
+    if (element.id === "btnTwo") {
+        modalTwo.style.display="block";
+    }
 }
+
 
 span.onclick = function () {
     modal.style.display = "none";
+    modalTwo.style.display = "none";
 }
 
 // when user clicks outside the modal, it closes
 window.onclick = function(e) {
     if (e.target == modal) {
         modal.style.display = "none";
+    }
+
+    if (e.target == modalTwo) {
+        modalTwo.style.display = "none";
     }
 }
